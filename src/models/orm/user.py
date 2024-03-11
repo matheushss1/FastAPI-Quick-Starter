8fcec0a7d5645a0a1c17ca1d7db9c216d4b656fe
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-11 19:20:54 +0000

    feat(models): add not-null scopes column to `users` table

--- src/models/orm/user.py
+++ src/models/orm/user.py
@@ -11,6 +11,7 @@ class User(Base):
     last_name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     hashed_password = Column(String, nullable=False)
+    scopes = Column(ARRAY(String), nullable=False)
 
 
 class UserInvited(Base):
