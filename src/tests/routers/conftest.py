eb7edfaaead63c38ad6e62ccdeb8f5b7acf962b3
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-11 19:12:44 +0000

    feat(conftest): add `superuser_fixture` with all scopes

--- src/tests/routers/conftest.py
+++ src/tests/routers/conftest.py
@@ -7,3 +7,22 @@ from sqlalchemy.orm import Session
 from src.managers.users import UserManager
 from src.models.orm.user import User
 
+PASSWORD = "testpass"
+
+
+@fixture(name="superuser")
+def superuser_fixture(session: Session) -> Generator[User, None, None]:
+    password_hash = UserManager(session).get_password_hash(PASSWORD)
+    user = User(
+        name="Test User",
+        last_name="From Tests",
+        email="testuser@test.com",
+        hashed_password=password_hash,
+        scopes=["users:self", "users:r", "users:rw", "users:all"],
+    )
+    session.add(user)
+    session.commit()
+    yield user
+    session.delete(user)
+    session.commit()
+
