9a8ecc9188b036428470e0bacdec18d26ad8cc86
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-12 17:38:19 +0000

    feat(security): add `OAUTH2_SCOPES` dict

---
+++ src/core/security.py
@@ -0,0 +1,6 @@
+OAUTH2_SCOPES = {
+    "users:self": "Read/Update self",
+    "users:r": "Read information about all users",
+    "users:rw": "Read and write information about all users",
+    "users:all": "All operations allowed for users",
+}
