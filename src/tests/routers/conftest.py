049bfd155c9f4931c34969fd4d86573303031bbd
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-12 15:15:50 +0000

    feat(conftest): sort imports

--- src/tests/routers/conftest.py
+++ src/tests/routers/conftest.py
@@ -3,7 +3,6 @@ from typing import Generator
 from fastapi.testclient import TestClient
 from pytest import fixture
 from sqlalchemy.orm import Session
-
 from src.managers.users import UserManager
 from src.models.orm.user import User
 
