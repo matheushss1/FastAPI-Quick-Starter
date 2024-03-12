558be886d351926d000a8eb6bc8ecc97fb4ccae9
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-12 17:40:24 +0000

    feat(utils): create `get_user_by_email` function

--- src/core/utils.py
+++ src/core/utils.py
@@ -7,3 +7,16 @@ from sqlalchemy.orm import Session
 from src.models.orm.user import User as UserORM
 from src.models.pydantic.user import User as UserPydantic
 
+
+def get_user_by_email(
+    db: Session, email: str, credentials_exception: HTTPException
+) -> UserPydantic | None:
+    user_db_list = db.query(UserORM).filter(UserORM.email == email).all()
+    if len(user_db_list):
+        return UserPydantic(
+            name=user_db_list[0].name,
+            email=user_db_list[0].email,
+            role=user_db_list[0].role,
+        )
+    raise credentials_exception
+
