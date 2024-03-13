38ba991e0c3767898daeef5e8e67ed4fcbabc0d2
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-13 21:44:19 +0000

    feat(repo): adjust initialize script to create roles and add them to the admin user

--- initialize.py
+++ initialize.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.core.database import Base, build_database_uri
 from src.managers.users import UserManager
+from src.models.orm.roles import MODES, MODULES, Role
 from src.models.orm.user import User
 
 INITIAL_PASSWORD = "password"
@@ -13,14 +14,45 @@ Base.metadata.create_all(bind=engine)
 
 def create_initial_user():
     db = SessionLocal()
-    if db.query(User).count() == 0:
+    if (
+        db.query(User).count() == 0
+        and db.query(Role)
+        .filter(Role.module == "users" and Role.mode == "all")
+        .count()
+        == 0
+    ):
+        print("Couldn't find any Role and User. Creating the first ones.")
+        roles = []
+        for module in MODULES:
+            for mode in MODES:
+                modes_roles_dict = {
+                    "all": "admin",
+                    "rw": "manager",
+                    "r": "user",
+                    "self": "member",
+                }
+                roles_description_dict = {
+                    "self": f"Read/Update own {module}",
+                    "r": f"Read information about all {module}",
+                    "rw": f"Read and write information about all {module}",
+                    "all": f"All operations allowed for {module}",
+                }
+                role = Role(
+                    name=modes_roles_dict.get(mode),
+                    description=roles_description_dict.get(mode),
+                    module=module,
+                    mode=mode,
+                )
+                roles.append(role)
+                db.add(role)
         password_hash = UserManager(db).get_password_hash(INITIAL_PASSWORD)
         admin_user = User(
             name="Admin",
             email="admin@admin.com",
             hashed_password=password_hash,
-            role="admin",
         )
+        for role in roles:
+            admin_user.roles.append(role)
         db.add(admin_user)
         db.commit()
         db.close()
