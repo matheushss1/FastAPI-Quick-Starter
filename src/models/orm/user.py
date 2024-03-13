6b76f8c8ca52ac033da03c087ca0e5f4b51e0de1
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-13 21:41:47 +0000

    feat(models): create association table `users_roles` and add the mapped `roles` to the `users` table. Also adjust imports.

--- src/models/orm/user.py
+++ src/models/orm/user.py
@@ -1,5 +1,16 @@
-from sqlalchemy import Column, DateTime, Enum, Integer, String
+from typing import List
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.core.database import Base
+from src.models.orm.roles import Role
+
+users_roles = Table(
+    "users_roles",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("role_id", ForeignKey("roles.id"), primary_key=True),
+)
 
 
 class User(Base):
@@ -9,6 +20,7 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     hashed_password = Column(String, nullable=False)
+    roles: Mapped[List["Role"]] = relationship(secondary=users_roles)
 
 
 class UserInvited(Base):
