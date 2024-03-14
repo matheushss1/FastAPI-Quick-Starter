198b7e35ebcb68833645fc3b2a8f3a04ed4bdf15
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-14 20:34:34 +0000

    feat(security): add imports

--- src/core/security.py
+++ src/core/security.py
@@ -1,6 +1,18 @@
-OAUTH2_SCOPES = {
-    "users:self": "Read/Update self",
-    "users:r": "Read information about all users",
-    "users:rw": "Read and write information about all users",
-    "users:all": "All operations allowed for users",
-}
+from contextlib import closing
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
+from sqlalchemy.orm import Session
+from src.core.dependencies import get_db, get_settings
+from src.core.utils import (
+    check_if_user_has_permissions,
+    get_all_roles,
+    get_credentials_exceptions,
+    get_email_by_decoded_jwt,
+    get_user_by_email,
+    get_user_scopes,
+    parse_scopes,
+)
+from src.models.pydantic.user import User
+
