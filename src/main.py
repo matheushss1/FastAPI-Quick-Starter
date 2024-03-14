e6d404fa76ecac9b1a0cc9ef3ee3fba49a98cb78
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-14 21:25:00 +0000

    feat(main): adjust imports

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, FastAPI
 from src.config.settings import Settings
-from src.core.dependencies import get_settings, oauth2_scheme  # noqa
+from src.core.dependencies import get_settings
+from src.core.security import oauth2_scheme  # noqa
 from src.routers.user import router as user_router
 
 settings = get_settings()
