cc1ccb6f5abf0bc982817b51cad812fa332b651d
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-14 20:46:18 +0000

    feat(utils): move there the helper functions and do some refactor

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,10 +1,12 @@
-from typing import Dict, List
+from typing import List
 
 from fastapi import HTTPException, status
 from fastapi.security import SecurityScopes
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
+from src.models.orm.user import Role as RoleORM
 from src.models.orm.user import User as UserORM
+from src.models.pydantic.role import Role as RolePydantic
 from src.models.pydantic.user import User as UserPydantic
 
 
@@ -13,10 +15,19 @@ def get_user_by_email(
 ) -> UserPydantic | None:
     user_db_list = db.query(UserORM).filter(UserORM.email == email).all()
     if len(user_db_list):
+        user_roles = [
+            RolePydantic(
+                name=role.name,
+                description=role.description,
+                module=role.module,
+                mode=role.mode,
+            )
+            for role in user_db_list[0].roles
+        ]
         return UserPydantic(
             name=user_db_list[0].name,
             email=user_db_list[0].email,
-            role=user_db_list[0].role,
+            roles=user_roles,
         )
     raise credentials_exception
 
@@ -52,23 +63,8 @@ def get_credentials_exceptions(
     )
 
 
-def get_user_scopes(role: str, oauth2_scopes=Dict[str, str]) -> List[str]:
-    all_scopes = list(oauth2_scopes.keys())
-    role_scopes = {
-        "admin": all_scopes,
-        "manager": [
-            scope for scope in all_scopes if scope.split(":")[-1] != "all"
-        ],
-        "user": [
-            scope
-            for scope in all_scopes
-            if scope.split(":")[-1] not in ["all", "rw"]
-        ],
-        "member": [
-            scope for scope in all_scopes if scope.split(":")[-1] == "self"
-        ],
-    }
-    return role_scopes.get(role)
+def get_user_scopes(roles: List[RolePydantic]) -> List[str]:
+    return list(parse_scopes(roles).keys())
 
 
 def get_email_by_decoded_jwt(
@@ -87,3 +83,45 @@ def get_email_by_decoded_jwt(
         raise credentials_exception
     finally:
         return email
+
+
+def generate_role_description(module: str, mode: str) -> str:
+    roles_description_dict = {
+        "self": f"Read/Update own {module}",
+        "r": f"Read information about all {module}",
+        "rw": f"Read/Update information about all {module}",
+        "all": f"All operations allowed for {module}",
+    }
+    return roles_description_dict.get(mode)
+
+
+def generate_role_name(mode: str) -> str:
+    modes_roles_dict = {
+        "all": "admin",
+        "rw": "manager",
+        "r": "user",
+        "self": "member",
+    }
+    return modes_roles_dict.get(mode)
+
+
+def get_all_roles(db: Session) -> List[RolePydantic]:
+    roles = db.query(RoleORM).all()
+    return [
+        RolePydantic(
+            name=role.name,
+            description=role.description,
+            module=role.module,
+            mode=role.mode,
+        )
+        for role in roles
+    ]
+
+
+def parse_scopes(roles: List[RolePydantic]) -> dict[str, str]:
+    scopes = {}
+    for role in roles:
+        scope_key = f"{role.module}:{role.mode}"
+        scope_value = role.description
+        scopes[scope_key] = scope_value
+    return scopes
