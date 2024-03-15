d0b0ae14ef55d47e91908adbbff24044d74e4ad1
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-15 18:20:00 +0000

    feat(tests): add tests to update and delete user

---
+++ src/tests/routers/test_user_routes.py
@@ -0,0 +1,46 @@
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+from src.models.orm.user import Role, User
+
+
+def test_update_user_name(
+    client: TestClient,
+    user_member: User,
+    user_member_token: str,
+    role_user_member: Role,
+):
+    update_dict = {"name": "User Testing Fixture Name"}
+    response = client.put(
+        f"/user/{user_member.id}",
+        headers={"Authorization": f"Bearer {user_member_token}"},
+        data=update_dict,
+    )
+    expected_response = {
+        "name": "User Testing Fixture Name",
+        "email": user_member,
+        "roles": [
+            {
+                "name": role_user_member.name,
+                "description": role_user_member.description,
+                "module": role_user_member.module,
+                "mode": role_user_member.mode,
+            }
+        ],
+    }
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+def test_delete_user(
+    client: TestClient,
+    session: Session,
+    user_member: User,
+    superuser_token: str,
+):
+    response = client.delete(
+        f"/user/{user_member.id}",
+        headers={"Authorization": f"Bearer {superuser_token}"},
+    )
+    user_query = session.query(User).where(User.id == user_member.id).all()
+    assert response.status_code == 204
+    assert not len(user_query)
