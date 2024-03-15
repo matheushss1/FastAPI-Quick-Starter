5311896b14d6a9b1318689f7dd2ae1dc5f3bf8ac
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-03-15 21:43:00 +0000

    feat(utils): adjust docstrings

--- src/managers/utils.py
+++ src/managers/utils.py
@@ -18,20 +18,18 @@ def get_db_single_object_by_email(
         db (Session): The SQLAlchemy database session.
         model (Any): The SQLAlchemy model representing the database table.
         email (str): The email address used to query the database.
-        exception (HTTPException): The HTTP exception to raise if the object is not found
-            of if found and expecting None.
-        expect_none (bool, optional): If True, expects no objects to be found and raises
-            the specified exception if an object is found. Defaults to False.
+        exception (HTTPException): The HTTP exception to raise if the object
+            is not found or if found and expecting None.
+        expect_none (bool, optional): If True, expects no objects to be found
+            and raises the specified exception if an object is found.
+            Defaults to False.
 
     Returns:
-        Union[Any, None]: The retrieved object if found, None if no object is found.
+        Union[Any, None]: The retrieved object if found, None if no object
+            is found.
 
     Raises:
         HTTPException: If the expect_none flag is False and no object is found.
-
-    Note:
-        This function assumes that the provided model has an 'email' attribute
-        representing the email address column in the corresponding database table.
     """
     objects = db.query(model).where(model.email == email).all()
     objects_len = len(objects)
@@ -48,7 +46,7 @@ def get_db_list_of_objects_by_list_of_ids(
     db: Session, model: Any, list_of_ids: List[int]
 ) -> List[Any]:
     """
-    Retrieves a list of objects from the database based on the provided list of IDs.
+    Retrieves a list of objects from the database based on the list of IDs.
 
     Args:
         db (Session): The SQLAlchemy database session.
@@ -56,7 +54,7 @@ def get_db_list_of_objects_by_list_of_ids(
         list_of_ids (List[int]): A list of integer IDs to filter the objects.
 
     Returns:
-        List[Any]: A list of objects from the database matching the provided IDs.
+        List[Any]: A list of objects from the database matching the IDs.
     """
     return db.query(model).where(model.id.in_(list_of_ids)).all()
 
@@ -75,20 +73,18 @@ def get_db_single_object_by_id(
         db (Session): The SQLAlchemy database session.
         model (Any): The SQLAlchemy model representing the database table.
         id (int): The int used to query the database.
-        exception (HTTPException): The HTTP exception to raise if the object is not found
-            of if found and expecting None.
-        expect_none (bool, optional): If True, expects no objects to be found and raises
-            the specified exception if an object is found. Defaults to False.
+        exception (HTTPException): The HTTP exception to raise if the object is
+            not found or if found and expecting None.
+        expect_none (bool, optional): If True, expects no objects to be found
+           and raises the specified exception if an object is found.
+           Defaults to False.
 
     Returns:
-        Union[Any, None]: The retrieved object if found, None if no object is found.
+        Union[Any, None]: The retrieved object if found,
+            None if no object is found.
 
     Raises:
         HTTPException: If the expect_none flag is False and no object is found.
-
-    Note:
-        This function assumes that the provided model has an 'email' attribute
-        representing the email address column in the corresponding database table.
     """
     objects = db.query(model).where(model.id == id).all()
     objects_len = len(objects)
